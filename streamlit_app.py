e5435484b14bf1df8d2c60919e7380c7413d73f4
thessing <37598026+thessing@users.noreply.github.com>
2025-07-31 21:55:30 -0400

    Update streamlit_app.py
    added token

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,8 @@ name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
 #session = get_active_session()
-cnx = st.connection("snowflake")
+mytoken="eyJraWQiOiIzNTU4OTQzMDQ1NjIzODE0IiwiYWxnIjoiRVMyNTYifQ.eyJwIjoiMjEyMTI5NTQwOjIxMjEyOTU0MCIsImlzcyI6IlNGOjEwNDkiLCJleHAiOjE3NTUzMDg4Nzh9.pjsljrCxAOQesq2DWTDwRfcUCif10s5pm-3mvb5FVm5_ehdknmxr-IeeL3XnNxbCEXXO9_YskjOjhYlWnvtHUA"
+cnx = st.connection("snowflake", token=mytoken)
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
