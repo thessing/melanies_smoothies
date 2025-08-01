cb4628447166ebeabb3136e112db234a9afed865
thessing <37598026+thessing@users.noreply.github.com>
2025-07-31 22:46:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 # Write directly to the app
 st.title(f"Customize Your Smoothie :cup_with_straw:")
@@ -24,7 +25,8 @@ st.write('The name on your Smoothie will be:', name_on_order)
 mytoken="eyJraWQiOiIzNTU4OTQzMDQ1NjIzODE0IiwiYWxnIjoiRVMyNTYifQ.eyJwIjoiMjEyMTI5NTQwOjIxMjEyOTU0MCIsImlzcyI6IlNGOjEwNDkiLCJleHAiOjE3NTUzMDg4Nzh9.pjsljrCxAOQesq2DWTDwRfcUCif10s5pm-3mvb5FVm5_ehdknmxr-IeeL3XnNxbCEXXO9_YskjOjhYlWnvtHUA"
 cnx = st.connection("snowflake", token=mytoken)
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON')
+pd_df = my_dataframe.to_pandas()
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections=5)
@@ -39,9 +41,13 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        st.subheader(fruit_chosen + ' Nutritional Information')
-        smoothiefroot_response = requests.get("https://www.fruityvice.com/api/fruit/"+fruit_chosen)
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+        search_on=pd_df.loc(pd_df['FRUIT_NAME']==fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+        if search_on:       
+            st.subheader(fruit_chosen + ' Nutritional Information')
+            smoothiefroot_response = requests.get("https://www.fruityvice.com/api/fruit/"+search_on)
+            sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
     
